e88d4c62a6214d8e250f9b14608b0550a20278a2
rahimabd13 <abdulrahim13501@gmail.com>
2023-06-19 01:09:42 +0530

    [Rahim]Added: Calculate Line Length

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            LengthOfLine lengthofline = new LengthOfLine(5, 4, 6, 3);
+
+            lengthofline.LengthCalculate();
+
+            Console.ReadLine();
+        }
+    }
+}
